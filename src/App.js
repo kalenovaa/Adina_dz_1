b3cb1584ed65a1f4ebe59de5390028b489718ef4
Asel <adelyakalenova@gmail.com>
2023-11-20 16:12:16 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, {useState} from "react";
+export default function MyApp() {
+  const [count, setCount] = useState(0)
+
+  function handleClick(){
+    setCount(count + 1)
+  }
+
+  return(
+      <div>
+        <h1>Lalalalalalalalal</h1>
+        <MyButton count={count} onClick={handleClick} />
+        <MyButton count={count} onClick={handleClick} />
+      </div>
+  )
+}
+
+function MyButton({count, onClick}) {
+  return(
+      <button onClick={onClick}>Clicked {count} times!</button>
+
+  )
+}
